b05736c56c1767ae888e3ebe22bf9bf3a43fb853
Gonzalo Corral <gonzalo.corral@edu.uah.es>
2022-08-22 20:43:13 +0200

    Fix loading of convnext pretrained weights

--- projects/convnext/build_models.py
+++ projects/convnext/build_models.py
@@ -16,6 +16,14 @@ def build_convnext(num_classes=1000, pretrained=True, **kwargs):
         checkpoint = torch.hub.load_state_dict_from_url(
             url=weights_url, map_location="cpu"
         )
-        model.load_state_dict(checkpoint["model"])
+        # We don't want to load the pretrained weights of the head of the
+        # network, as the number of output classes differ, so we delete those
+        # layers from the dict. Additionally we have to pass strict = False to
+        # load_state_dict, so that we can avoid an error when the
+        # 'head.weight/bias' keys are not present in the pretrained dict.
+        weights = checkpoint["model"]
+        del weights['head.weight']
+        del weights['head.bias']
+        model.load_state_dict(weights, strict=False)
 
     return model
