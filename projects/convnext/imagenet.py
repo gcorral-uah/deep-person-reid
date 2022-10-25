5a223007709e1cae069d07a208612df11be40838
Augur97 <gonza897@hotmail.com>
2022-10-25 22:10:14 +0200

    Wip to train imagenet

--- projects/convnext/imagenet.py
+++ projects/convnext/imagenet.py
@@ -1,3 +1,10 @@
+### TODO:
+# We should replace the library code from torch-reid with a natural one
+# Maybe use pythorch-lightning
+# We have another option using a data_manager from torchreid to do the work
+# - The data loaders should work
+# - The engine won't work as we haven't wrapped it in a data-manager
+
 import os
 import sys
 import time
@@ -57,14 +64,15 @@ def main():
     if cfg.use_gpu:
         torch.backends.cudnn.benchmark = True
 
-    datamanager = torchreid.data.ImageDataManager(**imagedata_kwargs(cfg))
     imagenet_args = {}
     train_loader, val_loader = imagenet.create_imagenet_data_loaders(imagenet_args)
+    imagenet_dataset_parameters = imagenet.imagenet_data()
 
     print("Building model: {}".format(cfg.model.name))
+    is_pretrained = False
     model = build_convnext(
-        num_classes=datamanager.num_train_pids,
-        pretrained=cfg.model.pretrained,
+        num_classes=imagenet_dataset_parameters["num_training_classes"],
+        pretrained=is_pretrained,
     )
     num_params, flops = compute_model_complexity(
         model, (1, 3, cfg.data.height, cfg.data.width)
--- projects/convnext/imagenet_data_loader/imagenet_preprocessed_data_loader.py
+++ projects/convnext/imagenet_data_loader/imagenet_preprocessed_data_loader.py
@@ -8,20 +8,28 @@ from torchvision.datasets import ImageFolder
 from augmentations import CutoutPIL
 
 
-def create_imagenet_data_loaders(args):
-    data_path_train = os.path.join(args.data_path, 'imagenet21k_train')
-    train_transform = transforms.Compose([
-        transforms.Resize((args.image_size, args.image_size)),
-        CutoutPIL(cutout_factor=0.5),
-        RandAugment(),
-        transforms.ToTensor(),
-    ])
+def imagenet_data():
+    return {"num_training_classes": 10450, "num_test_classes": 10450}
 
-    data_path_val = os.path.join(args.data_path, 'imagenet21k_val')
-    val_transform = transforms.Compose([
-        transforms.Resize((args.image_size, args.image_size)),
-        transforms.ToTensor(),
-    ])
+
+def create_imagenet_data_loaders(args):
+    data_path_train = os.path.join(args.data_path, "imagenet21k_train")
+    train_transform = transforms.Compose(
+        [
+            transforms.Resize((args.image_size, args.image_size)),
+            CutoutPIL(cutout_factor=0.5),
+            RandAugment(),
+            transforms.ToTensor(),
+        ]
+    )
+
+    data_path_val = os.path.join(args.data_path, "imagenet21k_val")
+    val_transform = transforms.Compose(
+        [
+            transforms.Resize((args.image_size, args.image_size)),
+            transforms.ToTensor(),
+        ]
+    )
 
     train_dataset = ImageFolder(data_path_train, transform=train_transform)
     val_dataset = ImageFolder(data_path_val, transform=val_transform)
@@ -33,12 +41,22 @@ def create_imagenet_data_loaders(args):
 
     # Pytorch Data loader
     train_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=args.batch_size, shuffle=sampler_train is None,
-        num_workers=args.num_workers, pin_memory=True, sampler=sampler_train)
+        train_dataset,
+        batch_size=args.batch_size,
+        shuffle=sampler_train is None,
+        num_workers=args.num_workers,
+        pin_memory=True,
+        sampler=sampler_train,
+    )
 
     val_loader = torch.utils.data.DataLoader(
-        val_dataset, batch_size=args.batch_size, shuffle=False,
-        num_workers=args.num_workers, pin_memory=False, sampler=sampler_val)
+        val_dataset,
+        batch_size=args.batch_size,
+        shuffle=False,
+        num_workers=args.num_workers,
+        pin_memory=False,
+        sampler=sampler_val,
+    )
 
     train_loader = PrefetchLoader(train_loader)
     val_loader = PrefetchLoader(val_loader)
@@ -54,7 +72,9 @@ class PrefetchLoader:
         first = True
         for batch in self.loader:
             with torch.cuda.stream(self.stream):  # stream - parallel
-                self.next_input = batch[0].cuda(non_blocking=True)  # note - (0-1) normalization in .ToTensor()
+                self.next_input = batch[0].cuda(
+                    non_blocking=True
+                )  # note - (0-1) normalization in .ToTensor()
                 self.next_target = batch[1].cuda(non_blocking=True)
 
             if not first:
