dcc52b07c5adc6e0c2baef4763e30ab3827ceaca
Augur97 <gonza897@hotmail.com>
2022-11-14 19:30:38 +0100

    Inline drop layer code in convnext_osnet

--- projects/convnext/convnext_impl/convnext_osnet.py
+++ projects/convnext/convnext_impl/convnext_osnet.py
@@ -9,7 +9,48 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from typing import List, Union, Optional
-import drop
+
+
+def drop_path(
+    x: torch.Tensor,
+    drop_prob: float = 0.0,
+    training: bool = False,
+    scale_by_keep: bool = True,
+):
+    """Drop paths (Stochastic Depth) per sample (when applied in main path of residual blocks).
+
+    This is the same as the DropConnect impl I created for EfficientNet, etc networks, however,
+    the original name is misleading as 'Drop Connect' is a different form of dropout in a separate paper...
+    See discussion: https://github.com/tensorflow/tpu/issues/494#issuecomment-532968956 ... I've opted for
+    changing the layer and argument names to 'drop path' rather than mix DropConnect as a layer name and use
+    'survival rate' as the argument.
+
+    """
+    if drop_prob == 0.0 or not training:
+        return x
+    keep_prob = 1 - drop_prob
+    shape = (x.shape[0],) + (1,) * (
+        x.ndim - 1
+    )  # work with diff dim tensors, not just 2D ConvNets
+    random_tensor = x.new_empty(shape).bernoulli_(keep_prob)
+    if keep_prob > 0.0 and scale_by_keep:
+        random_tensor.div_(keep_prob)
+    return x * random_tensor
+
+
+class DropPath(nn.Module):
+    """Drop paths (Stochastic Depth) per sample  (when applied in main path of residual blocks)."""
+
+    def __init__(self, drop_prob: float = 0.0, scale_by_keep: bool = True):
+        super(DropPath, self).__init__()
+        self.drop_prob = drop_prob
+        self.scale_by_keep = scale_by_keep
+
+    def forward(self, x: torch.Tensor):
+        return drop_path(x, self.drop_prob, self.training, self.scale_by_keep)
+
+    def extra_repr(self):
+        return f"drop_prob={round(self.drop_prob,3):0.3f}"
 
 
 class StackedConv(nn.Module):
@@ -195,7 +236,7 @@ class MultiBlock(nn.Module):
             if layer_scale_init_value > 0
             else None
         )
-        self.drop_path = drop.DropPath(drop_path) if drop_path > 0.0 else nn.Identity()
+        self.drop_path = DropPath(drop_path) if drop_path > 0.0 else nn.Identity()
 
     def forward(self, x: torch.Tensor):
         input = x
@@ -335,7 +376,7 @@ def build_convnext_osnet(num_classes=1000, pretrained=False, **kwargs):
         kernel_sizes=[4, 2, 7],
         kernel_strides=[4, 2, 1],
         kernel_paddings=[0, 0, 3],
-        **kwargs
+        **kwargs,
     )
     if pretrained:
         raise NotImplementedError
