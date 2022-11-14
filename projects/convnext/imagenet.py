28a08c568d4d7de7be0e50f75709987b546a9911
Gonzalo Corral <gonzalo.corral@edu.uah.es>
2022-11-14 13:12:46 +0100

    Update imagenet.py todo comment

--- projects/convnext/imagenet.py
+++ projects/convnext/imagenet.py
@@ -1,9 +1,8 @@
 ### TODO:
 # We should replace the library code from torch-reid with a natural one
 # Maybe use pythorch-lightning
-# We have another option using a data_manager from torchreid to do the work
-# - The data loaders should work
-# - The engine won't work as we haven't wrapped it in a data-manager
+# The saving and loading of the checkpoint is based on
+# https://web.archive.org/web/20201106104658/https://chshih2.github.io/blog/2020/10/06/Pytorch-continue-training/
 
 from convnext_impl.build_models import build_convnext
 import imagenet_data_loader.imagenet_preprocessed_data_loader as imagenet
