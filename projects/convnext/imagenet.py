491cdd8a907818c85779ae5b96414d49c521c1d8
Augur97 <gonza897@hotmail.com>
2022-12-18 17:13:53 +0100

    Add logging for debugging purposes

--- projects/convnext/imagenet.py
+++ projects/convnext/imagenet.py
@@ -4,6 +4,7 @@
 # The saving and loading of the checkpoint is based on
 # https://web.archive.org/web/20201106104658/https://chshih2.github.io/blog/2020/10/06/Pytorch-continue-training/
 
+DEBUG = True
 from convnext_impl.convnext_osnet import build_convnext_osnet
 import imagenet_data_loader.imagenet_preprocessed_data_loader as imagenet
 import torch
@@ -12,11 +13,154 @@ import glob
 import re
 import imagenet_data_loader.early_stopping as early_stopping
 
+## Note: This imports are for debuggin purpose only.
+if DEBUG:
+    from loguru import logger
+    import psutil
+    import atexit
+    import nvidia_smi
+
+    logger.add("file_{time}.log")
+
+    nvidia_smi.nvmlInit()
+    atexit.register(nvidia_smi.nvmlShutdown())
+
+    def memory_use():
+        mem = psutil.virtual_memory()
+        swap = psutil.virtual_memory()
+        available_memory = mem.available
+        used_memory_percent = mem.percent
+        available_swap = swap.available
+        used_swap_percent = swap.percent
+        mes = (
+            f"The system has {available_memory} free memory (used {used_memory_percent}%),"
+            + f"and {available_swap} swap (used {used_swap_percent}%)"
+        )
+        return mes
+
+    def disk_use(path):
+        disk_usage = psutil.disk_usage(path)
+        free_space = disk_usage.free
+        percent_used = disk_usage.used
+        mes = f"The disk that contains {path} has {free_space} free_space and is {percent_used}% full\n"
+        return mes
+
+    def cpu_use_detailed_nonblocking():
+        """Update and/or return the per CPU list using the psutil library."""
+        # The first value is bogus, as it meassures the cpu activity since the last call.
+        percpu_percent = []
+        for cpu_number, cputimes in enumerate(
+            psutil.cpu_times_percent(interval=0.0, percpu=True)
+        ):
+            cpu = {
+                "key": "f{cpu_number}",
+                "cpu_number": cpu_number,
+                "total": round(100 - cputimes.idle, 1),
+                "user": cputimes.user,
+                "system": cputimes.system,
+                "idle": cputimes.idle,
+            }
+            if hasattr(cputimes, "nice"):
+                cpu["nice"] = cputimes.nice
+            if hasattr(cputimes, "iowait"):
+                cpu["iowait"] = cputimes.iowait
+            if hasattr(cputimes, "irq"):
+                cpu["irq"] = cputimes.irq
+            if hasattr(cputimes, "softirq"):
+                cpu["softirq"] = cputimes.softirq
+            if hasattr(cputimes, "steal"):
+                cpu["steal"] = cputimes.steal
+            if hasattr(cputimes, "guest"):
+                cpu["guest"] = cputimes.guest
+            if hasattr(cputimes, "guest_nice"):
+                cpu["guest_nice"] = cputimes.guest_nice
+            percpu_percent.append(cpu)
+        return percpu_percent
+
+    def cpu_use_detailed_blocking_for_1sec():
+        """Update and/or return the per CPU list using the psutil library."""
+        # The first value is bogus, as it meassures the cpu activity since the last call.
+        percpu_percent = []
+        for cpu_number, cputimes in enumerate(
+            psutil.cpu_times_percent(interval=1.0, percpu=True)
+        ):
+            cpu = {
+                "key": "f{cpu_number}",
+                "cpu_number": cpu_number,
+                "total": round(100 - cputimes.idle, 1),
+                "user": cputimes.user,
+                "system": cputimes.system,
+                "idle": cputimes.idle,
+            }
+            if hasattr(cputimes, "nice"):
+                cpu["nice"] = cputimes.nice
+            if hasattr(cputimes, "iowait"):
+                cpu["iowait"] = cputimes.iowait
+            if hasattr(cputimes, "irq"):
+                cpu["irq"] = cputimes.irq
+            if hasattr(cputimes, "softirq"):
+                cpu["softirq"] = cputimes.softirq
+            if hasattr(cputimes, "steal"):
+                cpu["steal"] = cputimes.steal
+            if hasattr(cputimes, "guest"):
+                cpu["guest"] = cputimes.guest
+            if hasattr(cputimes, "guest_nice"):
+                cpu["guest_nice"] = cputimes.guest_nice
+            percpu_percent.append(cpu)
+        return percpu_percent
+
+    def cpu_use_blocking_1sec():
+        # Carefull this may sleep for one second. I think this has too much overhead.
+        lst = psutil.cpu_percent(interval=1.0, percpu=True)
+        usage_list = []
+        for idx, elem in enumerate(lst):
+            usage = f"Cpu {idx} has a usage of {elem}\n"
+            usage_list.append(usage)
+        res = "".join(usage_list)
+        return res
+
+    def cpu_use_nonblocking():
+        # The first value is bogus, as it meassures the cpu activity since the last call.
+        lst = psutil.cpu_percent(interval=0.0, percpu=True)
+        usage_list = []
+        for idx, elem in enumerate(lst):
+            usage = f"Cpu {idx} has a usage of {elem}\n"
+            usage_list.append(usage)
+        res = "".join(usage_list)
+        return res
+
+    def cpu_use():
+        return cpu_use_nonblocking()
+
+    def gpu_use():
+        if torch.cuda.is_available():
+            deviceCount = nvidia_smi.nvmlDeviceGetCount()
+            gpu_usage = []
+            for i in range(deviceCount):
+                handle = nvidia_smi.nvmlDeviceGetHandleByIndex(i)
+                info = nvidia_smi.nvmlDeviceGetMemoryInfo(handle)
+                usage = "Device {}: {}, Memory : ({:.2f}% free): {}(total), {} (free), {} (used) \n".format(
+                    i,
+                    nvidia_smi.nvmlDeviceGetName(handle),
+                    100 * info.free / info.total,
+                    info.total,
+                    info.free,
+                    info.used,
+                )
+                gpu_usage.append(usage)
+            return "".join(gpu_usage)
+        else:
+            return "No GPU avalible."
+
 
 def main():
     NUM_EPOCHS = 140
     MODEL_PATH = "convnext_imagenet.pth"
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+    if DEBUG:
+        logger.debug(f"Using Device {device}\n")
+
     imagenet_22k_loader_args = {
         # Path to the data (with folders imagenet21k_train and imagenet21k_val)
         "data_path": "/mnt/data1/gonzalo.corral/imagenet21k_resized/",
@@ -47,6 +191,15 @@ def main():
 
     train_loader, validation_loader = train_loader_1k, validation_loader_1k
 
+    if DEBUG:
+        if "train_loader_1k" in vars():
+            dataset_name = "imagenet_1k"
+        elif "train_loader_22k" in vars():
+            dataset_name = "imagenet_22k"
+        else:
+            dataset_name = ""
+        logger.debug(f"Loaded dataset {dataset_name}")
+
     print("Building model: {}")
     convnext_config_22k = {
         "num_classes": imagenet.imagenet_data_22k().get("num_training_classes", 10450),
@@ -76,6 +229,9 @@ def main():
             reverse=True,
         )
         latest_epoch = training_epochs[0]
+
+    if DEBUG and latest_epoch is not None:
+        logger.debug(f"Loading epoch: {latest_epoch}\n")
 
     best_validation_loss = None
     # Starting epoch (in case we need to restart training from a previous epoch)
@@ -109,7 +265,18 @@ def main():
 
         total_train_loss = 0.0
         for cur_train_iter, training_data in enumerate(train_loader):
-            print(f"In iteration: {cur_train_iter} and epoch: {epoch} of training loader")
+            print(
+                f"In iteration: {cur_train_iter} and epoch: {epoch} of training loader"
+            )
+
+            if DEBUG:
+                logger.debug(
+                    f"Entering iteration: {cur_train_iter} and epoch: {epoch} of training loader\n"
+                )
+                logger.debug(cpu_use())
+                logger.debug(memory_use())
+                logger.debug(gpu_use())
+
             data_inputs, data_labels = training_data
 
             # Move input data to device (only strictly necessary if we use GPU)
@@ -135,10 +302,16 @@ def main():
             # Calculate the total and average loss
             total_train_loss += loss
             avg_training_loss = total_train_loss / (cur_train_iter + 1)
+            if DEBUG:
+                logger.debug(
+                    f"Exiting iteration: {cur_train_iter} and epoch: {epoch} of training loader with avg_loss: {avg_training_loss}\n"
+                )
+                logger.debug(cpu_use())
+                logger.debug(memory_use())
+                logger.debug(gpu_use())
 
         print(f"I reached the end of training in epoch {epoch}")
         convnext_model.train(False)
-
 
         torch.save(
             {
@@ -154,7 +327,17 @@ def main():
         print(f"I am going to start validation of epoch: {epoch}")
         total_validation_loss = 0.0
         for cur_validation_iter, validation_data in enumerate(validation_loader):
-            print(f"In iteration: {cur_validation_iter} and epoch: {epoch} of validation loader")
+            print(
+                f"In iteration: {cur_validation_iter} and epoch: {epoch} of validation loader"
+            )
+            if DEBUG:
+                logger.debug(
+                    f"Entering iteration: {cur_validation_iter} and epoch: {epoch} of validation loader\n"
+                )
+                logger.debug(cpu_use())
+                logger.debug(memory_use())
+                logger.debug(gpu_use())
+
             validation_inputs, validation_labels = validation_data
             validation_inputs = validation_inputs.to(device)
             validation_labels = validation_labels.to(device)
@@ -178,14 +361,30 @@ def main():
             # to stop.
             earlystopping(avg_validation_loss, convnext_model)
             if earlystopping.early_stop:
+                if DEBUG:
+                    logger.debug(f"Early stopping in epoch {epoch}\n")
+                    logger.debug(cpu_use())
+                    logger.debug(memory_use())
+                    logger.debug(gpu_use())
 
                 print(f"Early stopping in epoch {epoch}")
                 # This only breaks out of validaton_loader. We also need to break from the main loop.
                 break
+
+            if DEBUG:
+                logger.debug(
+                    f"Exiting iteration: {cur_validation_iter} and epoch: {epoch} of validation loader\n"
+                )
+                logger.debug(cpu_use())
+                logger.debug(memory_use())
+                logger.debug(gpu_use())
+
         if earlystopping.early_stop:
             # Break of main loop if early stopping, as we don't want to continue with main loop.
             break
+
         print(f"I reached the end of validation in epoch {epoch}")
+        print(f"I have finished training")
 
 
 if __name__ == "__main__":
