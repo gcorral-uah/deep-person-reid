a7c347da98923bbe718fc4d516d0c0b00fd3c045
Augur97 <gonza897@hotmail.com>
2023-01-13 22:57:34 +0100

    More improvemnets to restart.sh

--- projects/convnext/restart.sh
+++ projects/convnext/restart.sh
@@ -3,19 +3,19 @@
 #killing. But we can simply orphan the process to avoid the surrounding loop
 #being terminated: See
 #https://superuser.com/questions/948718/restart-a-command-after-it-gets-killed-by-the-system
+# Another option may be
+# https://askubuntu.com/questions/832767/run-another-command-when-my-script-receives-sigkill-signal
+# Yeet another option may be to simply start the script without nohup, which
+# seems to cause problems. In testing the OS doesn't kill the parent when a
+# child recives a SIGKILL or SIGABORT (kill -9/-15).
 while true; do
     if [[ -e "finished.txt" ]]; then
         break
     fi
-    if [[ -e "nohup.out" ]]; then
-        rm "nohup.out"
-    fi
     # conda run -n imagenet-training nohup python imagenet.py
     # nohup conda run -n imagenet-training python imagenet.py
-    source ~/miniconda3/etc/profile.d/conda.sh
-    conda activate imagenet-training
-    nohup python imagenet.py 2> "nohup.err"
-    pid=$!
-    wait $pid || continue
-    break
+    # source ~/miniconda3/etc/profile.d/conda.sh
+    # conda activate imagenet-training
+    # python imagenet.py 2> "nohup.err"
+    conda run -n imagenet-training python imagenet.py 
 done
