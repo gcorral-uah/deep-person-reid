503c6941ef514049597f7f43088910f805c20a09
Augur97 <gonza897@hotmail.com>
2023-01-13 19:37:21 +0100

    Try to make the restart script more robust

--- projects/convnext/restart.sh
+++ projects/convnext/restart.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
+#The OOM Killer sends SIGKILL, so there's no way to gracefully handle the
+#killing. But we can simply orphan the process to avoid the surrounding loop
+#being terminated: See
+#https://superuser.com/questions/948718/restart-a-command-after-it-gets-killed-by-the-system
 while true; do
     if [[ -e "finished.txt" ]]; then
         break
     fi
-    python imagenet.py
+    conda activate imagenet-training
+    nohup python imagenet.py
+    pid=$!
+    wait $pid || continue
+    break
 done
