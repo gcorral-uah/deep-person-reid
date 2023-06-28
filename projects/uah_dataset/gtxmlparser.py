5084066b443e76a52fc04b556dca669f77b8cfc7
gcorral-uah <gonzalo.corral@edu.uah.es>
2023-06-28 22:23:22 +0200

    Add function to generate the xml from the gt files using octave

--- projects/uah_dataset/gtxmlparser.py
+++ projects/uah_dataset/gtxmlparser.py
@@ -1,9 +1,60 @@
 from collections import defaultdict
+from os.path import exists
 from typing import Dict
 import xml.dom.minidom
 import glob
+import subprocess
 import os
+import glob
 import re
+import shutil
+
+def generate_frames(folder: str) -> None:
+    # Expand the ~
+    folder = os.path.expanduser(folder)
+    command = ["bash", "generate_frames.sh"]
+    dir = folder
+    subprocess.run(command, cwd=dir)
+
+def generate_xml_using_octave(folder: str, remove_xml: bool = False) -> None:
+    # Expand the ~
+    folder = os.path.expanduser(folder)
+
+    print(f"Entering to process and generate xml on folder {folder=}")
+
+    # Example of calling the function on octave
+    # gt2xml('~/gba/2016_video003/video3.gt', '~/gba_/2016_video003/FRAMES', '~/gba/2016_video003/xml')
+    gt_file_name = glob.glob("*.gt",root_dir=folder)
+    assert len(gt_file_name) == 1
+
+    dir = folder + '/' if folder[-1] != '/' else folder
+    gt_file = dir + gt_file_name[0]
+    frames_dir = dir + "FRAMES"
+    xml_dir = dir + "xml"
+
+    if not remove_xml and os.path.exists(xml_dir):
+        # If we are not recreating the files and the folder exists, we can skip it.
+        return
+
+    if remove_xml:
+        print(f"Removing dir {xml_dir}")
+        shutil.rmtree(xml_dir, ignore_errors=True)
+
+    command = ["octave", "--eval", f"gt2xml('{gt_file}', '{frames_dir}', '{xml_dir}')"]
+    subprocess.run(command)
+
+def generate_all_xml_of_dataset(folder: str) -> None:
+    # Expand the ~
+    folder = os.path.expanduser(folder)
+    subfolders = [ f.path for f in os.scandir(folder) if f.is_dir() ]
+
+    for dir in subfolders:
+        print(f"Entering {dir=}")
+        # The other dir contain files that are not video, so ignore it.
+        if re.match(".*other.*", dir):
+            continue
+
+        generate_xml_using_octave(dir)
 
 
 def parse_gt_xml_file(file: str) -> list[int]:
